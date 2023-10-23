49794edd481261c1a2911f07e1f8f50fa8e75096
rlego <lagynkov.rom@gmail.com>
2023-10-23 13:58:07 +0300

    CLI

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "utils.h"
 
-int main() {
+int main(int argc, char** argv){
+    if (argc < 3) { return -1; }
     crypto_utils::CryptoEngine engine;
-    engine.choose_type(crypto_utils::TypeCryptographic::XOR);
-    engine.choose_action(crypto_utils::TypeAction::Encrypt);
-    utils::process_in_directory("B:/CProjects/Env", engine);
-    engine.choose_action(crypto_utils::TypeAction::Decrypt);
-    utils::process_in_directory("B:/CProjects/Env", engine);
+    if (argv[1] == "c") {
+
+        engine.choose_type(crypto_utils::TypeCryptographic::XOR);
+        engine.choose_action(crypto_utils::TypeAction::Encrypt);
+        utils::process_in_directory(argv[2], engine);
+    }
+    if (argv[1] == "d") {
+        engine.choose_type(crypto_utils::TypeCryptographic::XOR);
+        engine.choose_action(crypto_utils::TypeAction::Decrypt);
+        utils::process_in_directory(argv[2], engine);
+    }
+
     return 0;
 }
