9afe29cd14c5b18b238abe678adea7ee17a327a6
rlego <lagynkov.rom@gmail.com>
2023-10-23 14:08:02 +0300

    cli fixes

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 int main(int argc, char** argv){
     if (argc < 3) { return -1; }
     crypto_utils::CryptoEngine engine;
-    if (argv[1] == "c") {
-
+    std::string flag (argv[1]);
+    if (flag == "e") {
         engine.choose_type(crypto_utils::TypeCryptographic::XOR);
         engine.choose_action(crypto_utils::TypeAction::Encrypt);
         utils::process_in_directory(argv[2], engine);
     }
-    if (argv[1] == "d") {
+    if (flag == "d") {
         engine.choose_type(crypto_utils::TypeCryptographic::XOR);
         engine.choose_action(crypto_utils::TypeAction::Decrypt);
         utils::process_in_directory(argv[2], engine);
